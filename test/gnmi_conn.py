1d6cf19d5f202370f041a9293901b5aaf8f49366
uminisutetoke <hyakunichisou@outlook.jp>
2025-08-06 06:38:59 +0000

    insecure

--- test/gnmi_conn.py
+++ test/gnmi_conn.py
@@ -1,16 +1,20 @@
 from pygnmi.client import gNMIclient
 
 # Variables
-host = ('172.20.20.4', '57400')
+host1 = {"ip_address": "172.80.80.21", "port": 57401,
+        "username": "admin", "password": "NokiaSrl1!"}
 
 # Body
 if __name__ == '__main__':
     with gNMIclient(
-        target=host,
-        username='admin',
-        password='NokiaSrl1!',
-        insecure=True
+        target=(host1["ip_address"], host1["port"]),
+        username=host1["username"],
+        password=host1["password"],
+        # debug=True,
+        insecure=True,
+        skip_verify=True,
         ) as gc:
-            result = gc.get(path=['openconfig-interfaces:interfaces', 'openconfig-acl:acl'])
+        result = gc.capabilities()
+        #result = gc.get(path=['/system/grpc-server[name=insecure-mgmt]/'])
 
     print(result)
