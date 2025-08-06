719951baeb58a80b08f7777c2af7ac1dba12e324
uminisutetoke <hyakunichisou@outlook.jp>
2025-08-06 05:32:57 +0000

    ch_topology

--- test/gnmi_conn.py
+++ test/gnmi_conn.py
@@ -1,39 +1,16 @@
-import asyncio
 from pygnmi.client import gNMIclient
 
-# --- 監視対象の情報をここに書く ---
-TARGET_HOST = "srl1"  # ContainerLabでのコンテナ名
-TARGET_PORT = 57400
-TARGET_USER = "admin"
-TARGET_PASS = "admin"
-# ------------------------------------
+# Variables
+host = ('172.20.20.4', '57400')
 
-async def check_device():
-    """指定した機器にgNMIで接続できるか試す"""
-    print(f"--- {TARGET_HOST}への接続テストを開始します ---")
+# Body
+if __name__ == '__main__':
+    with gNMIclient(
+        target=host,
+        username='admin',
+        password='NokiaSrl1!',
+        insecure=True
+        ) as gc:
+            result = gc.get(path=['openconfig-interfaces:interfaces', 'openconfig-acl:acl'])
 
-    # 接続情報を作成
-    target = (TARGET_HOST, TARGET_PORT)
-
-    try:
-        # gNMIクライアントの部品を作成
-        client = gNMIclient(target=target,
-                              username=TARGET_USER,
-                              password=TARGET_PASS,
-                              insecure=True)
-
-        # client部品を使って、capabilities()という機能をお願いする
-        # この瞬間に接続とリクエストが行われます
-        response = await client.capabilities()
-
-        print("✅ 接続成功！")
-        print(f"gNMIバージョン: {response.gNMI_version}")
-
-    except Exception as e:
-        # 接続に失敗した場合
-        print(f"❌ 接続失敗...")
-        print(f"エラー内容: {e}")
-
-# --- プログラムの実行 ---
-if __name__ == "__main__":
-    asyncio.run(check_device())
+    print(result)
